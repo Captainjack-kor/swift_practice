29bf420676de83accbebc860a267e0392aef591d
Jack Lim <jennystr233@gmail.com>
2022-01-04 22:47:48 +0900

    git commit test

---
+++ switf_test/Shared/switf_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  switf_testApp.swift
+//  Shared
+//
+//  Created by Jack Lim on 1/4/22.
+//
+
+import SwiftUI
+
+@main
+struct switf_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
